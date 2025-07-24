9c349361c0d2cef360616883ec6ca5027dbf7ab1
Karthikgv07 <155318530+Karthikgv07@users.noreply.github.com>
2025-07-24 22:18:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import seaborn as sns
 st.title("Export Clearance Approval Prediction App")
 
 # Load cleaned data
-df = pd.read_csv("PEC---app/Cleaned_transaction_Data.csv")
+df = pd.read_csv("Cleaned_Transaction_Data.csv")
 
 # Load model & pipeline
 model = pickle.load(open("model/rf_smote_model.pkl", "rb"))
